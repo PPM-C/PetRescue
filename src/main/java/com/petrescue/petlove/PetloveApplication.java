5827179e75fa9779c1347b67bfb24a80ae2a7420
pamelapalacios <palacios.pamela@gmail.com>
2025-09-10 09:38:13 +0200

    haciendo pruebas

---
+++ src/main/java/com/petrescue/petlove/PetloveApplication.java
@@ -0,0 +1,13 @@
+package com.petrescue.petlove;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetloveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetloveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/petrescue/petlove/PetloveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petrescue.petlove;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetloveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
